ee38b65e4d314569e9fa4f80fe3492c2ea506ff6
Sebastian Pirvan <149628713+Pirvan-Sebastian@users.noreply.github.com>
2023-11-27 19:00:40 +0200

    Add files via upload

--- Wordle.dev/main.cpp
+++ Wordle.dev/main.cpp
@@ -1,14 +1,11 @@
 //          Wordle-C++ 
 #include<bits/stdc++.h>
 #include <conio.h>
+#include "functii.h"
 
-#define RESET   "\033[0m"
-#define RED     "\033[31m"
-#define GREEN   "\033[32m"
-#define YELLOW  "\033[33m"
+
 
 using namespace std;
-ifstream fin("cuvinte.txt");
 
 //declarare variabile
 string linie;
@@ -16,100 +13,42 @@ int numar_aparitii,numar_random,Frecventa[27];
 int numar_lini,i,j,ok,corect,s,castig;
 string cuvant_user,cuvant_corect,varianta;
 
+
 int main()
 {
 
 srand(time(0));
 numar_random = rand() % 10658;
 
-while (fin>>linie)
-{
-    numar_lini++;
-    if (numar_lini == numar_random)
-    {
-        cuvant_corect=linie; 
-        break;
-    }
-}
+cuvant_corect=alegere_cuvant(numar_random);
 
 cout<<"             WORDLE             "<<endl;
 cout<<endl;
 int lungime_cuvant=cuvant_corect.length();
 
 //Vector frecventa
-for(i=0;i<lungime_cuvant;i++)
-{
-  Frecventa[cuvant_corect[i]-'a']++;
-}
+for(i=0;i<lungime_cuvant;i++) Frecventa[cuvant_corect[i]-'a']++;
 
 int incercari=lungime_cuvant+1;
 
 while(castig==0)
 {
-//introducere cuvant de la user
- while(corect==0)
-  {
-    cout<<"Introduceti un cuvant de "<<lungime_cuvant<<" litere: ";
-    cin>>cuvant_user;
-    cout<<endl;
-    if(lungime_cuvant!=cuvant_user.length())
-      cout<<"Cuvantul introdus nu are lungimea corecta"<<endl;
-    else
-      {
-        corect=1; 
-        incercari--;
-      }
-  }
-//verific litera cu litera si formez varianta
- for(i=0;i<lungime_cuvant;i++)
-  { ok=0; 
-
-    for(j=0;j<lungime_cuvant;j++)
-      {
-       if(cuvant_user[i]==cuvant_corect[j] && i==j)
-       {
-        ok=2;
-        numar_aparitii++;
-        break;
-       }
-       else if(cuvant_user[i]==cuvant_corect[j] && i!=j)
-         {
-             ok=1;
-             numar_aparitii++;
-         }
-         if(numar_aparitii>=Frecventa[cuvant_user[i]-'a']) ok=0;
-      }
-
-      varianta[i]=ok;
-  }
-
-  //verificare varianta introdusa de user
-  for(i=0;i<lungime_cuvant;i++)
-    {
-      if(varianta[i]==2)
-       cout << GREEN <<cuvant_user[i]<< RESET;
-      else if(varianta[i]==1)
-         cout << YELLOW <<cuvant_user[i]<< RESET;
-      else
-         cout << RED <<cuvant_user[i]<< RESET;
-    }
-
+citire_cuvant(lungime_cuvant,incercari,corect,cuvant_user);
+verificare_cuvant(lungime_cuvant,Frecventa,cuvant_user,cuvant_corect,varianta);
+verificare_varianta(lungime_cuvant,cuvant_user,varianta);
     //verific daca e solutie finala
-        for(i=0;i<lungime_cuvant;i++)
-          {
-          s+=varianta[i];
-          }
-          if(s==2*lungime_cuvant)
-            {
+  for(i=0;i<lungime_cuvant;i++) s+=varianta[i];
+        if(s==2*lungime_cuvant)
+           {
+             cout<<endl;
+             castig=2;
+             break;
+           }
+        else
+           {
               cout<<endl;
-              castig=2;break;
+              cout<<"Mai aveti "<<incercari<<" incercari"<<endl;
             }
-            else
-              {
-                cout<<endl;
-                cout<<"Mai aveti "<<incercari<<" incercari"<<endl;
-              }
-
         if(incercari==0)
           {
           castig=1;break;
@@ -118,16 +57,7 @@ while(castig==0)
     //resetez variabilele
       corect=0;s=0;
     }
-
-    if(castig==1)
-      {
-      cout<<"Ati pierdut! Cuvantul era: "<<cuvant_corect;
-      }
-    else if(castig==2)
-      {
-      cout<<"Felicitari! Ati castigat!";
-      }
-
+verificare_castig(castig,cuvant_corect);
 //Oprire la apasarea tastei Enter
       char ch;
       cout<<endl<<"Apasati Enter pentru a iesi din program";
@@ -136,6 +66,6 @@ while(castig==0)
       ch = getch();
      }while( ch != char(13) );//13 cod ASCII pt Enter
 
-      return 0;
-    
+
+  return 0;
 }
